fc99342ea6a9798dfecd032826df2687df4dd44f
Z008FT2 <Chad.Moller@target.com>
2022-09-16 11:12:42 -0500

    Additional properties to control plugin and JVM versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "health-monitor-interface"
+
+pluginManagement {
+    val kotlinPluginVersion: String by settings
+    val nexusPublishPluginVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinPluginVersion
+        id("io.github.gradle-nexus.publish-plugin") version nexusPublishPluginVersion
+    }
+}
